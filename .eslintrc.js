d839eff37db2ba80b4ea7333a653f952b275dd73
james <jpb12@hotmail.co.uk>
2017-04-28 20:14:01 +0100

    Fixing build

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,10 +19,6 @@ module.exports = {
             "tab",
 			{ "SwitchCase": 1 }
         ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
         "quotes": [
             "error",
             "single"
